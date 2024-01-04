855ed9f52f56f1a85cdb03cd5b4a04fb9d64c4c7
ImranKov <imran.kovacevic@yahoo.com>
2024-01-04 11:54:44 +0100

    zadatak broj x

---
+++ app.js
@@ -0,0 +1,15 @@
+let pozdravi = [
+    "Zdravo!",
+    "Pozdrav!",
+    "Dobar dan!",
+    "Dobro jutro!",
+    "Dobra večer!",
+    "Ćao!",
+    "Hello!",
+    "Alo!",
+    "Eee!",
+    "Selam Alejkum!"
+  ];
+  for (let i = 0; i < 8; i++) {
+    console.log(pozdravi[i]);
+  }
